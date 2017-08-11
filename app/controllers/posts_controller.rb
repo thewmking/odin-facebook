34a84f9b32870e2e7631c6819a717f665fea2ad1
Matthew King <thewmking@gmail.com>
2017-08-11 16:50:53 -0400

    Add post validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :correct_user, only: :destroy
 
   def new
   end
@@ -11,7 +12,7 @@ class PostsController < ApplicationController
     else
       flash[:danger] = "Error creating post."
     end
-    redirect_to root_url
+    redirect_to request.referrer
   end
 
   def index
@@ -23,12 +24,21 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+    flash[:success] = "Post deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    flash[:danger] = "Not authorized"
+    redirect_to request.referrer if @post.nil?
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+validates :content, presence: true, length: { maximum: 5000 }
+
 belongs_to :user
 has_many :likes
 
